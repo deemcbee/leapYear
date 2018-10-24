36ecf9f474dcc8cf55b7690814851100ef02d25b
deemcbee <djmcbri1@asu.edu>
2018-10-24 15:21:03 -0700

    Initial Code
    Adding the inital code for Leap year

---
+++ leapYear.py
@@ -0,0 +1,10 @@
+def isLeapYear (year):
+	if (year-1800) % 4 == 0:
+		print("This is a leap year")
+	else:
+		print("This is not a leap year")
+
+testYear = int(input("Please enter a year after 1800: "))
+
+isLeapYear(testYear)
+
